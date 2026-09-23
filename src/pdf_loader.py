63e5c6952b25a09046ed6152ddf518204a9b0f44
iamSriman <srim78344@example.com>
2026-09-23 19:45:22 +0530

    completed the pdf loader

---
+++ src/pdf_loader.py
@@ -0,0 +1,13 @@
+import fitz
+def load_pdf(file_path):
+    document= fitz.open(file_path)
+    text=""
+    for page in document:
+        text+=page.get_text()
+    document.close()
+    return text
+
+if __name__ == "__main__":
+    text = load_pdf("data/2024-wttc-introduction-to-ai.pdf")
+
+    print(text)
