84f64222a4c189e7fde472fb21583ed92a505841
iamSriman <srim78344@example.com>
2026-09-23 23:30:56 +0530

    complited retriever

---
+++ src/retriever.py
@@ -0,0 +1,19 @@
+from embedding import model
+from vector_store import collection
+
+def retrieve(query, top_k=3):
+    query_embedding= model.encode([query])
+    results=collection.query(
+        query_embeddings=query_embedding.tolist(),
+        n_results=top_k
+    )
+    return results
+
+if __name__=="__main__":
+    query="what is supervised learning?"
+    results=retrieve(query, top_k=3)
+    print("\nRetrieved documents:\n")
+
+    for document in results:
+        print("--------------------------------")
+        print(document)
